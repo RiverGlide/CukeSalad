0f65a51dae8a82e269f93ac8d11a763818b9ffda
Antony Marcano <antony@antonymarcano.com>
2011-03-05 12:37:38 +0000

    A Place To Start: do something with information //am,ap,jm

--- Examples/Calculator/features/APlaceToStart.feature
+++ Examples/Calculator/features/APlaceToStart.feature
@@ -4,5 +4,10 @@ Feature: A Place To Start
 	So that I know when I can start calculating
 
 Scenario: Start with Zero
-	When I switch on the calculator
-	Then I should see it display 0
+	When I attempt to switch on the calculator
+	Then I should see it display "0"
+	
+Scenario: Enter a number
+	Given I was able to switchon the calculator
+	When I attempt to enter "1"
+	Then I should see it display "1"
--- Examples/Calculator/features/step_definitions/APlaceToStart_steps.rb
+++ Examples/Calculator/features/step_definitions/APlaceToStart_steps.rb
@@ -1,16 +1,29 @@
 class Calculator
   
-  def display
-    0
+  attr_reader :display
+  
+  def initialize
+    @display = 0
+  end
+  
+  def enter value
+    @display = value
   end
   
 end  
 
-When /^I switch on the calculator$/ do
+Given /^I was able to ([^"]*)$/ do |something|
   @calculator = Calculator.new
 end
-  
-Then "I should see it display 0" do
-  @calculator.display.should == 0
+
+When /^I attempt to ([^"]*)$/ do |something|
+  @calculator = Calculator.new
 end
-  
+
+When /^I attempt to ([^"]*) "([^"]*)"$/ do |something,with_information|
+  @calculator.send(something.downcase.to_sym, with_information.to_i)
+end
+
+Then /^I should see it display "([^"]*)"$/ do |arg1|
+  @calculator.display.should == arg1.to_i
+end
