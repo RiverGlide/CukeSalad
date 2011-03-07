b3e92fde085d08f8dece297640178f741065e3a6
Antony Marcano <antony@antonymarcano.com>
2011-03-07 11:03:57 +0000

    Addition: Tasks a married to relevant information. Getting crowded in here - things need to find a new home. //@antonymarcano, @jamesmartin

--- Examples/Calculator/features/step_definitions/CalculatingIndividual_steps.rb
+++ Examples/Calculator/features/step_definitions/CalculatingIndividual_steps.rb
@@ -41,14 +41,35 @@ class Actor
     @role = role.new
   end
   
-  def perform task, with_information = nil
-    if with_information.nil?
-      @role.send method_for( task )
+  def perform task
+    if task.information != nil
+      @role.send task.name, task.information
     else
-      @role.send method_for( task ) , arguments_from( with_information )
+      @role.send task.name
     end
   end
   alias :answer :perform
+end
+
+class Task
+  attr_reader :name, :information
+  
+  def initialize name, information = nil
+    @name=name
+    @information = information
+  end
+end
+
+class SubjectMatterExpert
+  
+  def how_do_i_perform this_task, with_information =nil
+    if with_information != nil
+      Task.new method_for( this_task ), arguments_from( with_information )
+    else
+      Task.new method_for( this_task )
+    end
+  end
+  alias :how_do_i_answer :how_do_i_perform
   
   def method_for something
     something.downcase.gsub(" ","_").to_sym 
@@ -59,16 +80,23 @@ class Actor
   end
 end
 
-When /^I (?:attempt to|was able to)? ([^']*)$/ do |task|
+When /^I (?:attempt to|was able to)? ([^']*)$/ do |this_task|
   @actor = @actor ||= Actor.new(CalculatingIndividual)
+  @sme = @sme ||= SubjectMatterExpert.new
+  task = @sme.how_do_i_perform this_task
   @actor.perform task
 end
 
-When /^I attempt to ([^']*) '(.*)'$/ do |task, with_information|
+When /^I attempt to ([^']*) '(.*)'$/ do |this_task, with_information|
   @actor = @actor ||= Actor.new(CalculatingIndividual)
-  @actor.perform task, with_information
+  @sme = @sme ||= SubjectMatterExpert.new
+  task = @sme.how_do_i_perform this_task, with_information
+  @actor.perform task
 end
 
-Then /^I should ([^']*) '([^']*)'$/ do |question, expect_value|
+Then /^I should ([^']*) '([^']*)'$/ do |this_question, expect_value|
+  @actor = @actor ||= Actor.new(CalculatingIndividual)
+  @sme = @sme ||= SubjectMatterExpert.new
+  question = @sme.how_do_i_answer this_question
   @actor.answer( question ).to_s.should == expect_value
 end
