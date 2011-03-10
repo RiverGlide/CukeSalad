ea67b533840717796fd112162125034690df456f
Antony Marcano <antony+github@antonymarcano.com>
2011-03-10 20:15:29 +0000

    Refactoring: Librarian deals with the name error... now can replace Narrative Librarian //@antonymarcano

--- Examples/Calculator/spec/librarian_spec.rb
+++ Examples/Calculator/spec/librarian_spec.rb
@@ -10,15 +10,18 @@ class MoreThanOneWord
 end
 
 class Find
+  def Find.class_for something
+    begin 
+      Kernel.const_get ( class_name_from something )
+    rescue NameError
+      raise "The Librarian couldn't find the class '#{class_name_from something}'.\nMaybe you need to create it."
+    end
+  end
 
-
-  def Find.class_name_from this_sentence
+ def Find.class_name_from this_sentence
     joined_together capitalised( words_from this_sentence )
   end
-  def Find.class_for something
-    Kernel.const_get ( class_name_from something )
-  end
-
+  
   def Find.joined_together words
     words.inject :+
   end
@@ -47,6 +50,10 @@ describe "Something" do
   it "finds a class with more than one word" do
     the_thing_we_found = Find.class_for "more than one word"
     the_thing_we_found.should == MoreThanOneWord
+  end
+
+  it "complains when it encounters a name error" do
+    lambda { Find.class_for "something that isnt there" }.should raise_error RuntimeError, "The Librarian couldn't find the class 'SomethingThatIsntThere'.\nMaybe you need to create it."
   end
 
 end
