253142747fb5249f84e224a2ece2873363abe0ab
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 16:55:23 +0000

    Refactoring: The Actor can perform a simple task //@antonymarcano

--- Examples/Calculator/spec/actor_spec.rb
+++ Examples/Calculator/spec/actor_spec.rb
@@ -10,20 +10,40 @@ end
 class Actor
 
   def initialize role, director=Director.new
-    director.how_do_i_perform role
+    @director = director
+    @character = @director.how_do_i_perform role
   end
-
+  
+  def perform task_description
+    task = @director.how_do_i_perform task_description
+    task.perform_as @character
+  end
 end
 
 describe Actor do
 
-  it "finds out how to perform a role" do
+  it "gets into character" do
     director = double("director")
-    role = "description of the role"
-    director.should_receive( :how_do_i_perform ).with( role )
+    role_description = "description of the role"
+    role = double( role_description )
+    director.should_receive( :how_do_i_perform ).with( role_description ).and_return( role )
     
-    actor = Actor.new role, director
+    actor = Actor.new role_description, director
   end  
 
+  it "performs a simple task as that character" do
+    director = double("director")
+    role_description = "description of the role"
+    role = double( role_description )
+    director.should_receive( :how_do_i_perform ).with( role_description ).and_return( role )
+    actor = Actor.new role_description, director
+   
+    task_description = "some description of a task"
+    task = double( task_description )
+    task.should_receive( :perform_as ).with( role )
+    director.should_receive( :how_do_i_perform ).with( task_description ).and_return( task )
+
+    actor.perform task_description
+  end
 end
 
