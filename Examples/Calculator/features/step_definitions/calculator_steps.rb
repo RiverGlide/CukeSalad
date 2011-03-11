c14e90189ecff0e793d639c31a3b0b7b70235604
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 12:03:16 +0000

    Refactoring: Cleaned up the calculator_steps a little //@antonymarcano,@jamesmartin

--- Examples/Calculator/features/step_definitions/calculator_steps.rb
+++ Examples/Calculator/features/step_definitions/calculator_steps.rb
@@ -1,5 +1,5 @@
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
 $:.unshift(File.dirname(__FILE__) + '/../tasks')
 
-require 'calculating_individual' #TODO: remove the need for require so that the Narrative_steps can come from a gem
 require 'narrative_steps'
+require 'calculating_individual' 
