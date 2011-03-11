c6c6b0cbb296e243af0637d2ddc6a9a8eca52b5f
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 17:57:34 +0000

    Refactoring: making the spec for the Director make a little more sense //@antonymarcano

--- Examples/Calculator/spec/director_spec.rb
+++ Examples/Calculator/spec/director_spec.rb
@@ -23,13 +23,10 @@ describe Director do
   
   it "tells you how to perform a role" do
     researcher = double("researcher")
-    something = "description of the thing"
+    something = "description of the task or role"
     directives = SomeTaskOrRole
-     
-
     director = Director.new researcher
     researcher.should_receive( :get_directives_for ).with( something ).and_return( directives )
-
 
     director.how_do_i_perform( something ).should be_instance_of directives
   end
