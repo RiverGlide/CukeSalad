b9212f80e0267258276155c283963ab45721a862
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 14:03:26 +0000

    Refactoring: The actor asks the subject matter expert how to perform a role or a task in that role //@antonymarcano

--- Examples/Calculator/lib/Narrative_steps.rb
+++ Examples/Calculator/lib/Narrative_steps.rb
@@ -2,11 +2,13 @@ require 'ostruct'
 require 'librarian'
 
 class Actor
-  def initialize role
-    @character = role.new
+  def initialize role_description
+    @sme = SubjectMatterExpert.new
+    @character = @sme.how_do_i_perform role_description
   end
   
-  def perform task
+  def perform this_task, *with_these_details
+    task = @sme.how_do_i_perform this_task, *with_these_details
     task.perform_as @character
   end
   alias :answer :perform
@@ -14,18 +16,18 @@ end
 
 class SubjectMatterExpert
   include Librarian  
-  def how_do_i_perform this_task, *these_details 
-    task = class_for( this_task )
-    if task_requires_details?(task) 
-      task.new(*with_arguments_from(these_details))
+  def how_do_i_perform this_something, *these_details 
+    something = class_for( this_something )
+    if details_required_for?(something) 
+      something.new(*with_arguments_from(these_details))
     else
-      task.new 
+      something.new 
     end
   end
   alias :how_do_i_answer :how_do_i_perform
     
-  def task_requires_details? task
-    task.instance_method(:initialize).arity > 0 
+  def details_required_for? something
+    something.instance_method(:initialize).arity > 0 
   end
 
   def with_arguments_from details
@@ -41,20 +43,17 @@ Before do
 end
 
 Given /^I am a ([a-zA-Z ]+)$/ do |role|
-  @actor = Actor.new(@sme.class_for role)
+  @actor = Actor.new(role)
 end
 
 When /^I (?:attempt to|was able to)? ([^']*)$/ do |this_task|
-  task = @sme.how_do_i_perform this_task
-  @actor.perform task
+  @actor.perform this_task
 end
 
 When /^I (?:attempt to|was able to)? ([A-Z a-z_-]*): (.*)$/ do |this_task, with_these_details|
-  task = @sme.how_do_i_perform this_task, with_these_details
-  @actor.perform task
+  @actor.perform this_task, with_these_details
 end
 
 Then /^I should ([^']*) '([^']*)'$/ do |this_question, expect_value|
-  question = @sme.how_do_i_answer this_question
-  @actor.answer( question ).to_s.should == expect_value
+  @actor.answer( this_question ).to_s.should == expect_value
 end
