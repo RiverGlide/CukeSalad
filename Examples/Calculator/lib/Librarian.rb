b5580b3acb8ecf6786742688c6acb768235e3f8f
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 11:03:22 +0000

    Refactoring: We have a Librarian :-) //@antonymarcano, @jamesmartin

---
+++ Examples/Calculator/lib/Librarian.rb
@@ -0,0 +1,26 @@
+module Librarian
+  def class_for something
+    begin 
+      Kernel.const_get( class_name_from something )
+    rescue NameError
+      raise "The Librarian couldn't find the class '#{class_name_from something}'.\nMaybe you need to create it."
+    end
+  end
+
+ def class_name_from this_sentence
+    joined_together capitalised( words_from this_sentence )
+  end
+  
+  def joined_together words
+    words.join
+  end
+
+  def words_from sentence
+   sentence.split(" ")
+  end
+
+  def capitalised words
+    words.collect { |word | word.capitalize }
+  end
+end
+
--- Examples/Calculator/spec/librarian_spec.rb
+++ Examples/Calculator/spec/librarian_spec.rb
@@ -1,6 +1,7 @@
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require 'rubygems'
 require 'bundler'
+require 'librarian'
 Bundler.setup
 
 class Something
@@ -10,51 +11,33 @@ end
 class MoreThanOneWord
 end
 
-class Find
-  def Find.class_for something
-    begin 
-      Kernel.const_get ( class_name_from something )
-    rescue NameError
-      raise "The Librarian couldn't find the class '#{class_name_from something}'.\nMaybe you need to create it."
-    end
-  end
-
- def Find.class_name_from this_sentence
-    joined_together capitalised( words_from this_sentence )
-  end
-  
-  def Find.joined_together words
-    words.join
-  end
-
-  def Find.words_from sentence
-   sentence.split(" ")
-  end
-
-  def Find.capitalised words
-    words.collect { |word | word.capitalize }
-  end
+class Expert
+  include Librarian
 end
   
 describe "Something" do
 
+  before :each do
+    @expert = Expert.new
+  end
+
   it "finds a class for a thing" do
-    the_thing_we_found = Find.class_for "Something"
+    the_thing_we_found = @expert.class_for "Something"
     the_thing_we_found.should == Something  
   end
 
   it "finds a class for another" do
-    the_thing_we_found = Find.class_for "Another"
+    the_thing_we_found = @expert.class_for "Another"
     the_thing_we_found.should == Another
   end
 
   it "finds a class with more than one word" do
-    the_thing_we_found = Find.class_for "more than one word"
+    the_thing_we_found = @expert.class_for "more than one word"
     the_thing_we_found.should == MoreThanOneWord
   end
 
   it "complains when it encounters a name error" do
-    lambda { Find.class_for "something that isnt there" }.should raise_error RuntimeError, "The Librarian couldn't find the class 'SomethingThatIsntThere'.\nMaybe you need to create it."
+    lambda { @expert.class_for "something that isnt there" }.should raise_error RuntimeError, "The Librarian couldn't find the class 'SomethingThatIsntThere'.\nMaybe you need to create it."
   end
 
 end
