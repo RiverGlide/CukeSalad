f880071a0f00cd06de3df7d2e8546486c490f8c4
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 17:06:48 +0000

    Refactoring: Now our actor can answer a simple question //@antonymarcano

--- Examples/Calculator/spec/actor_spec.rb
+++ Examples/Calculator/spec/actor_spec.rb
@@ -18,6 +18,7 @@ class Actor
     task = @director.how_do_i_perform task_description
     task.perform_as @character
   end
+  alias :answer :perform
 end
 
 describe Actor do
@@ -40,10 +41,27 @@ describe Actor do
    
     task_description = "some description of a task"
     task = double( task_description )
-    task.should_receive( :perform_as ).with( role )
     director.should_receive( :how_do_i_perform ).with( task_description ).and_return( task )
+    task.should_receive( :perform_as ).with( role )
 
     actor.perform task_description
   end
+
+  it "can perform a task that answers a question" do
+    director = double("director")
+    role_description = "description of the role"
+    role = double( role_description )
+    director.should_receive( :how_do_i_perform ).with( role_description ).and_return( role )
+    actor = Actor.new role_description, director
+   
+    some_question = "some question to ask"
+    the_answer = "the answer to the question"
+    question = double( some_question )
+    director.should_receive( :how_do_i_perform ).with( some_question ).and_return( question )
+    question.should_receive( :perform_as ).with( role ).and_return( the_answer )
+
+    actor.answer( some_question ).should == the_answer
+  end
+
 end
 
