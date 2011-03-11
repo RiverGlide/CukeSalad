31b79b22bf6eba7cd3c58677884038ab189945e7
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 18:34:47 +0000

    Refactoring: Director has moved to new accommodation //@antonymarcano

---
+++ Examples/Calculator/lib/director.rb
@@ -0,0 +1,16 @@
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+require 'rubygems'
+require 'bundler'
+require 'researcher'
+Bundler.setup
+
+class Director
+
+  def initialize researcher=Researcher.new
+    @researcher = researcher
+  end
+
+  def how_do_i_perform this_thing, *with_details
+    @researcher.get_directives_for( this_thing ).new *with_details
+  end
+end
