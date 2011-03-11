30ba34f8a182f1937b778675306585b8ee641dbd
Antony Marcano <antony+github@antonymarcano.com>
2011-03-11 18:00:58 +0000

    Refactoring: The director can say how to answer a question too //@antonymarcano

--- Examples/Calculator/spec/director_spec.rb
+++ Examples/Calculator/spec/director_spec.rb
@@ -16,12 +16,13 @@ class Director
   def how_do_i_perform this_thing
     @researcher.get_directives_for( this_thing ).new
   end
+  alias :how_do_i_answer :how_do_i_perform
 end
 
 
 describe Director do
   
-  it "tells you how to perform a role" do
+  it "tells you how to perform something like a task or a role" do
     researcher = double("researcher")
     something = "description of the task or role"
     directives = SomeTaskOrRole
@@ -29,5 +30,15 @@ describe Director do
     researcher.should_receive( :get_directives_for ).with( something ).and_return( directives )
 
     director.how_do_i_perform( something ).should be_instance_of directives
+  end
+
+  it "tells you how to get the answer to a question" do
+    researcher = double("researcher")
+    something = "description of the task or role"
+    directives = SomeTaskOrRole
+    director = Director.new researcher
+    researcher.should_receive( :get_directives_for ).with( something ).and_return( directives )
+
+    director.how_do_i_answer( something ).should be_instance_of directives
   end
 end
