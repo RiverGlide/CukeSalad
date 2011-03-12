b280365695752bb1b919f13d4cab2b60e0f74c03
Antony Marcano <antony+github@antonymarcano.com>
2011-03-12 10:32:39 +0000

    Refactoring: TaskWithSpecifics only puts underscores instead of spaces if the info is meant to be a name //@antonymarcano

--- Examples/Calculator/lib/task_with_specifics.rb
+++ Examples/Calculator/lib/task_with_specifics.rb
@@ -5,18 +5,44 @@ require 'ostruct'
 Bundler.setup
 
 class TaskWithSpecifics
-  def initialize detail
-    @info = with_specifics_from( detail )
+  def initialize details
+    @info = with_specifics_from( details )
   end
   
   def value_of(symbol)
     @info.send symbol
   end
 
-  def with_specifics_from detail
-    specifics_pattern = /('[^']+')/
-    OpenStruct.new Hash[*detail.split(specifics_pattern).collect { |e| e.gsub('\'', '').strip.gsub(' ', '_') }]
+  def with_specifics_from details
+    OpenStruct.new Hash[*names_and_values_in(details).collect { |e|
+      item = e.strip
+      if is_a_name? item
+        replace_spaces_with_underscores_in(item)
+      elsif is_a_value? item 
+        remove_surrounding_quotes_from(item)
+      end
+      }]
   end
 
+  def replace_spaces_with_underscores_in item
+    item.gsub(' ', '_')
+  end
+
+  def remove_surrounding_quotes_from item 
+        item.gsub(/^'|'$/, '')
+  end
+
+  def is_a_name? item
+    item =~ /^[^'](?:.*)[^']$/
+  end
+  
+  def is_a_value? item
+    item =~ /^'(?:.*)'$/ 
+  end
+
+  def names_and_values_in details
+    specifics_pattern = /('[^']+')/
+    details.split(specifics_pattern)
+  end
 end
 
--- Examples/Calculator/spec/task_with_specifics_spec.rb
+++ Examples/Calculator/spec/task_with_specifics_spec.rb
@@ -28,10 +28,14 @@ describe TaskWithSpecifics do
   end
   
   it "should cope with values having more than one word" do
-    pending "Underscores should not be added to values with more than one word"
+    #pending "Underscores should not be added to values with more than one word"
     task = MyTask.new "first thing 'item' second thing 'another thing'"
     task.value_of(:first_thing).should == "item"
     task.value_of(:second_thing).should == "another thing"
   end
-  
+
+  def names_and_values_in details
+    specifics_pattern = /('[^']+')/
+    details.split(specifics_pattern)
+  end
 end
