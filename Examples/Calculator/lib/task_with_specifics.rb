d3499b62efd1a021e79f3344521806aa09d817d6
James Martin <jimmymartin@gmail.com>
2011-03-12 17:26:20 +1100

    Refactoring: TaskWithSpecifics now takes a single detail and @specifics refers to a single OpenStruct

--- Examples/Calculator/lib/task_with_specifics.rb
+++ Examples/Calculator/lib/task_with_specifics.rb
@@ -5,15 +5,13 @@ require 'ostruct'
 Bundler.setup
 
 class TaskWithSpecifics
-  def initialize *details
-    @specifics = *with_specifics_from( details )
+  def initialize detail
+    @specifics = with_specifics_from( detail )
   end
 
-  def with_specifics_from details
+  def with_specifics_from detail
     specifics_pattern = /('[^']+')/
-    details.collect do | detail |
-      OpenStruct.new Hash[*detail.split(specifics_pattern).collect { |e| e.gsub('\'', '').strip.gsub(' ', '_') }]
-    end
+    OpenStruct.new Hash[*detail.split(specifics_pattern).collect { |e| e.gsub('\'', '').strip.gsub(' ', '_') }]
   end
 
 end
