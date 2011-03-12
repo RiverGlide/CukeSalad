c673f87bffbae83169e8039a39cabe492424c118
Antony Marcano <antony+github@antonymarcano.com>
2011-03-12 10:50:50 +0000

    Narrative steps have been put in their place //@antonymarcano

--- Examples/Calculator/features/step_definitions/calculator_steps.rb
+++ Examples/Calculator/features/step_definitions/calculator_steps.rb
@@ -1,5 +1,6 @@
 $:.unshift(File.dirname(__FILE__) + '/../../lib')
 $:.unshift(File.dirname(__FILE__) + '/../tasks')
+$:.unshift(File.dirname(__FILE__) + '/../../../../lib')
 
 require 'narrative_steps'
 require 'calculating_individual' 
