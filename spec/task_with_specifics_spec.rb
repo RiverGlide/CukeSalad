c8d88586fbcc368d4667b7aaae67366fbe390f2a
Antony Marcano <antony+github@antonymarcano.com>
2011-03-13 16:38:39 +0000

    Refactoring: Removing unnecessary code // @antonymarcano, @andypalmer

--- spec/task_with_specifics_spec.rb
+++ spec/task_with_specifics_spec.rb
@@ -25,14 +25,8 @@ describe TaskWithSpecifics do
   end
   
   it "should cope with values having more than one word" do
-    #pending "Underscores should not be added to values with more than one word"
     task = MyTask.new "first thing 'item' second thing 'another thing'"
     task.value_of(:first_thing).should == "item"
     task.value_of(:second_thing).should == "another thing"
-  end
-
-  def names_and_values_in details
-    specifics_pattern = /('[^']+')/
-    details.split(specifics_pattern)
   end
 end
