8b6768ab09c62f3246a67d4cfaf968d7385e605a
Antony Marcano <antony+github@antonymarcano.com>
2011-03-13 15:17:00 +0000

    Refactoring: Improving readability some more // @antonymarcano, @andypalmer

--- lib/task_with_specifics.rb
+++ lib/task_with_specifics.rb
@@ -8,37 +8,23 @@ class TaskWithSpecifics
   end
 
   def with_specifics_from details
-    Hash[*name_value_pairs_from(names_and_values_in(details))]
+    result = {}
+    names_and_values_in(details).each_slice(2) do |name_value| 
+      result[symbolized name_value[0]] = the_value_from_the name_value[1]
+    end
+    result
   end
 
-  def name_value_pairs_from names_and_values
-    names_and_values.collect { |e| give_me_a_something e.strip }
-  end
-
-  def give_me_a_something item
-    return replace_spaces_with_underscores_in(item).to_sym if is_a_name? item
-    return remove_surrounding_quotes_from(item) if is_a_value? item
-    raise "How did we get here?"
-  end
- 
   def names_and_values_in details
     specifics_pattern = /('[^']+')/
     details.split(specifics_pattern)
   end
 
-  def replace_spaces_with_underscores_in item
-    item.gsub(' ', '_')
+  def symbolized name
+    name.strip.gsub(' ', '_').to_sym
   end
 
-  def remove_surrounding_quotes_from item 
+  def the_value_from_the item 
     item.gsub(/(^'|'$)/, '')
-  end
-
-  def is_a_name? item
-    item =~ /^[^'].*[^']$/
-  end
-  
-  def is_a_value? item
-    item =~ /^'.*'$/ 
   end
 end
