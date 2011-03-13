1e3ad29cfe1de250a0e27372f64f89d2663ab0bc
Antony Marcano <antony+github@antonymarcano.com>
2011-03-13 14:16:47 +0000

    Refactoring: Improving readability // @antonymarcano, @andypalmer

--- lib/task_with_specifics.rb
+++ lib/task_with_specifics.rb
@@ -12,14 +12,18 @@ class TaskWithSpecifics
   end
 
   def name_value_pairs_from names_and_values
-    names_and_values.collect { |e|
-      item = e.strip
-      if is_a_name? item
-        replace_spaces_with_underscores_in(item).to_sym
-      elsif is_a_value? item 
-        remove_surrounding_quotes_from(item)
-      end
-    }
+    names_and_values.collect { |e| give_me_a_something e.strip }
+  end
+
+  def give_me_a_something item
+    return replace_spaces_with_underscores_in(item).to_sym if is_a_name? item
+    return remove_surrounding_quotes_from(item) if is_a_value? item
+    raise "How did we get here?"
+  end
+ 
+  def names_and_values_in details
+    specifics_pattern = /('[^']+')/
+    details.split(specifics_pattern)
   end
 
   def replace_spaces_with_underscores_in item
@@ -37,10 +41,4 @@ class TaskWithSpecifics
   def is_a_value? item
     item =~ /^'.*'$/ 
   end
-
-  def names_and_values_in details
-    specifics_pattern = /('[^']+')/
-    details.split(specifics_pattern)
-  end
 end
-
