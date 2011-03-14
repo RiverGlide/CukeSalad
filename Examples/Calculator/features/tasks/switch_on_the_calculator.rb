6b45901331fa61d652d3dc7147722d9725183443
James Martin <jimmymartin@gmail.com>
2011-03-15 08:13:23 +1100

    A Place to Start: Switching on the Calculator defines a contract with the Calculating Individual

--- Examples/Calculator/features/tasks/switch_on_the_calculator.rb
+++ Examples/Calculator/features/tasks/switch_on_the_calculator.rb
@@ -5,5 +5,8 @@ class SwitchOnTheCalculator
   
   def perform_as calculating_individual
       calculating_individual.should_not be_nil
+      calculating_individual.should respond_to :enters
+      calculating_individual.should respond_to :presses
+      calculating_individual.should respond_to :can_see
   end
 end
