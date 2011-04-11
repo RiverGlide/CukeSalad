6d519dfe4a34026f4884c9a57c11bedd5d3b8173
Antony Marcano <antony+github@antonymarcano.com>
2011-04-11 20:50:53 +0100

    Some readability improvements

--- lib/task_author.rb
+++ lib/task_author.rb
@@ -1,9 +1,9 @@
 require 'codify/as_const_name'
-def in_order_to do_something, *map, &block
-  attr_map = map[0]
+def in_order_to do_something, *with_attributes, &actions
+  attr_map = with_attributes[0]
   name = Codify::ConstName.from do_something
   m = Module.new do
-    define_method :perform_task, &block
+    define_method :perform_task, &actions
     define_method :the do | value |
       value_of( attr_map.key value )
     end
--- spec/codify/as_const_name_spec.rb
+++ spec/codify/as_const_name_spec.rb
@@ -3,25 +3,26 @@ require 'spec_helper'
 require 'codify/as_const_name'
 
 describe Codify::ConstName do
-
+  include Codify
+  
   it "gives you ClassName from when it's already a class name" do
-    Codify::ConstName.from( "ClassName" ).should == "ClassName"
+    ConstName.from( "ClassName" ).should == "ClassName"
   end
 
   it "gives you ClassName when separated by spaces" do
-    Codify::ConstName.from( "Class Name" ).should == "ClassName"
+    ConstName.from( "Class Name" ).should == "ClassName"
   end
 
   it "gives you ClassName from lowercase class name" do
-    Codify::ConstName.from( "class name" ).should == "ClassName"
+    ConstName.from( "class name" ).should == "ClassName"
   end
 
   it "gives you ClassName from lower case class name with leading/trailing spaces" do
-    Codify::ConstName.from( " class name " ).should == "ClassName"
+    ConstName.from( " class name " ).should == "ClassName"
   end
   
   it "formats as a ClassName from a mix of class format and lower case" do
-    Codify::ConstName.from( "AnotherClass name " ).should == "AnotherClassName"
+    ConstName.from( "AnotherClass name " ).should == "AnotherClassName"
   end
 end
 
