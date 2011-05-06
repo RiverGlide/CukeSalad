70f9204cef9eb64f8bb9edc2c148229fb2ec968a
Jon Rowe <mail@jonrowe.co.uk>
2011-05-06 14:07:14 +0100

    move this into its own module, defined so it can be accessed either by inclusion of TaskAuthor, or as TaskAuthor.in_order_to

--- lib/cukesalad/task_author.rb
+++ lib/cukesalad/task_author.rb
@@ -1,12 +1,21 @@
 require 'cukesalad/codify/const_name'
-def in_order_to do_something, *with_attributes, &actions
-  attr_map = with_attributes[0]
-  name = Codify::ConstName.from do_something
-  m = Module.new do
-    define_method :perform_task, &actions
-    define_method :the do | value |
-      value_of( attr_map.key value )
+module CukeSalad
+  module TaskAuthor
+
+    def in_order_to do_something, *with_attributes, &actions
+      TaskAuthor.in_order_to do_something, *with_attributes, &actions
+    end
+
+    def TaskAuthor.in_order_to do_something, *with_attributes, &actions
+      attr_map = with_attributes[0]
+      name = Codify::ConstName.from do_something
+      m = Module.new do
+        define_method :perform_task, &actions
+        define_method :the do | value |
+          value_of( attr_map.key value )
+        end
+      end
+      Kernel.const_set name, m
     end
   end
-  Kernel.const_set name, m
 end
--- spec/cukesalad/task_author_spec.rb
+++ spec/cukesalad/task_author_spec.rb
@@ -13,37 +13,40 @@ class SomeActor
   end
 end
 
-describe 'TaskAuthor' do
+module CukeSalad
+  describe 'TaskAuthor' do
+    include TaskAuthor
 
-  it "creates a module by the same name" do
-    in_order_to "SomeTask" do;end
+    it "creates a module by the same name" do
+      in_order_to "SomeTask" do;end
 
-    SomeActor.new.see_how_to_do SomeTask
-  end
-
-  it "creates a module by a similar name" do
-    in_order_to "some different task" do;end
-
-    SomeActor.new SomeDifferentTask
-  end
-
-  it "enables the actor to do something" do
-    in_order_to "SomeOtherTask" do
-      "done"
+      SomeActor.new.see_how_to_do SomeTask
     end
 
-    actor = SomeActor.new
-    actor.see_how_to_do SomeOtherTask
-    actor.do_your_thing.should == "done"
-  end
+    it "creates a module by a similar name" do
+      in_order_to "some different task" do;end
 
-  it "maps some attributes" do
-    in_order_to "YetAnotherTask", with_info: :info do
-      the :info
+      SomeActor.new SomeDifferentTask
     end
 
-    actor = SomeActor.new
-    actor.see_how_to_do YetAnotherTask
-    actor.do_your_thing.should == "this is the info"
+    it "enables the actor to do something" do
+      in_order_to "SomeOtherTask" do
+        "done"
+      end
+
+      actor = SomeActor.new
+      actor.see_how_to_do SomeOtherTask
+      actor.do_your_thing.should == "done"
+    end
+
+    it "maps some attributes" do
+      in_order_to "YetAnotherTask", with_info: :info do
+        the :info
+      end
+
+      actor = SomeActor.new
+      actor.see_how_to_do YetAnotherTask
+      actor.do_your_thing.should == "this is the info"
+    end
   end
 end
